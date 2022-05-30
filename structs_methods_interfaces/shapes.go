d4ed6bdff5fc6f461d8824733a8f2d395cfe9192
Artem <101756515+LightBulbfromSpace@users.noreply.github.com>
2022-05-30 13:39:57 +0200

    finished chapter "structs, methods & interfaces"

--- structs_methods_interfaces/shapes.go
+++ structs_methods_interfaces/shapes.go
@@ -1,11 +1,72 @@
 package perimeter
 
 import (
+	"math"
+)
+
+type Shape interface {
+	Perimeter() float64
+	Area() float64
+}
+
+type Square struct {
+	side float64
+}
+
+func (s Square) Perimeter() float64 {
+	return s.side * 4
+}
+
+func (s Square) Area() float64 {
+	return math.Pow(s.side, 2)
+}
+
+type Rectangle struct {
+	width  float64
+	height float64
+}
+
+func (rect Rectangle) Perimeter() float64 {
+	return (rect.width + rect.height) * 2
+}
+
+func (rect Rectangle) Area() float64 {
+	return rect.width * rect.height
+}
+
+type RightTriangle struct {
+	a float64
+	b float64
+}
+
+func (t RightTriangle) Perimeter() float64 {
+	return t.a + t.b + math.Sqrt(t.a*t.a+t.b*t.b)
+}
+
+func (t RightTriangle) Area() float64 {
+	return (t.a * t.b) / 2
+}
+
+type Circle struct {
+	rad float64
+}
+
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.rad
+}
+
+func (c Circle) Area() float64 {
+	return math.Pow(c.rad, 2) * math.Pi
+}
+
+/*func Perimeter(nameOfFigure string, l ...float64) float64 {
+=======
 	"fmt"
 	"math"
 )
 
 func Perimeter(nameOfFigure string, l ...float64) float64 {
+>>>>>>> f59ce94d2e9982d8d48a01c2906e2296dad90863
 	switch nameOfFigure {
 	case "square":
 		return l[0] * 4
@@ -17,9 +78,15 @@ func Perimeter(nameOfFigure string, l ...float64) float64 {
 		fmt.Print("This shape is not available or one hasn't been specified\n")
 		return 0
 	}
+<<<<<<< HEAD
+}*/
+
+/*func Area(nameOfFigure string, l ...float64) float64 {
+=======
 }
 
 func Area(nameOfFigure string, l ...float64) float64 {
+>>>>>>> f59ce94d2e9982d8d48a01c2906e2296dad90863
 	switch nameOfFigure {
 	case "square":
 		return l[0] * l[0]
@@ -31,4 +98,5 @@ func Area(nameOfFigure string, l ...float64) float64 {
 		fmt.Print("This shape is not available or one hasn't been specified\n")
 		return 0
 	}
-}
+<<<<<<< HEAD
+}*/
--- structs_methods_interfaces/shapes_test.go
+++ structs_methods_interfaces/shapes_test.go
@@ -1,61 +1,48 @@
 package perimeter
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestPerimeter(t *testing.T) {
-	check := func(t testing.TB, got, want float64) {
-		t.Helper()
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
+	perimeterTests := []struct {
+		name  string
+		shape Shape
+		want  float64
+	}{
+		{name: "Square", shape: Square{side: 10.0}, want: 40.0},
+		{name: "Rectangle", shape: Rectangle{width: 30.0, height: 40.0}, want: 140.0},
+		{name: "RightTriangle", shape: RightTriangle{a: 3.0, b: 4.0}, want: 12.0},
+		{name: "Circle", shape: Circle{rad: 10.0}, want: 62.83185307179586},
 	}
-	t.Run("test for square", func(t *testing.T) {
-		got := Perimeter("square", 10.0)
-		want := 40.0
-		check(t, got, want)
-	})
-	t.Run("test for rectangle", func(t *testing.T) {
-		got := Perimeter("rectangle", 10.0, 20.0)
-		want := 60.0
-		check(t, got, want)
-	})
-	t.Run("test for right triangle", func(t *testing.T) {
-		got := Perimeter("right triangle", 3.0, 4.0)
-		want := 12.0
-		check(t, got, want)
-	})
-	t.Run("test for another cases", func(t *testing.T) {
-		got := Perimeter("else", 3.0, 4.0, 10.0)
-		want := 0.0
-		check(t, got, want)
-	})
+
+	for _, figt := range perimeterTests {
+		t.Run(figt.name, func(t *testing.T) {
+			got := figt.shape.Perimeter()
+			if got != figt.want {
+				t.Errorf("got %g want %g", got, figt.want)
+			}
+		})
+	}
 }
 
 func TestArea(t *testing.T) {
-	check := func(t testing.TB, got, want float64) {
-		t.Helper()
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
+	areaTests := []struct {
+		name  string
+		shape Shape
+		want  float64
+	}{
+		{name: "Square", shape: Square{side: 10.0}, want: 100.0},
+		{name: "Rectangle", shape: Rectangle{width: 10.0, height: 20.0}, want: 200.0},
+		{name: "RightTriangle", shape: RightTriangle{3.0, 4.0}, want: 6.0},
+		{name: "Circle", shape: Circle{rad: 10.0}, want: 314.1592653589793},
 	}
-	t.Run("test for square", func(t *testing.T) {
-		got := Area("square", 10.0)
-		want := 100.0
-		check(t, got, want)
-	})
-	t.Run("test for rectangle", func(t *testing.T) {
-		got := Area("rectangle", 10.0, 20.0)
-		want := 200.0
-		check(t, got, want)
-	})
-	t.Run("test for right triangle", func(t *testing.T) {
-		got := Area("right triangle", 3.0, 4.0)
-		want := 6.0
-		check(t, got, want)
-	})
-	t.Run("test for another cases", func(t *testing.T) {
-		got := Area("else", 3.0, 4.0, 10.0)
-		want := 0.0
-		check(t, got, want)
-	})
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.want {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.want)
+			}
+		})
+	}
 }
