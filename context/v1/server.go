f308a175b14ec5a45128d36cbafb6b0b4da96bb5
LightBulbfromSpace <potschta.00@mail.ru>
2022-07-22 18:41:34 +0200

    ...

---
+++ context/v1/server.go
@@ -0,0 +1,40 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Store interface {
+	Fetch() string
+	Cancel()
+}
+
+type StubStore struct {
+	response string
+}
+
+func (s *StubStore) Fetch() string {
+	return s.response
+}
+
+func (s *StubStore) Cancel() {}
+
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		data := make(chan string, 1)
+
+		go func() {
+			data <- store.Fetch()
+		}()
+
+		select {
+		case d := <-data:
+			fmt.Fprint(w, d)
+
+		case <-ctx.Done():
+			store.Cancel()
+		}
+	}
+}
